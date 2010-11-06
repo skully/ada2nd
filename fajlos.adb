631c7ae1e28d10aa4b69e37cda0539a73f82761f
skully <skully@elte.hu>
2010-11-06 20:17:43 +0100

    na mar beletotogeti az ertekeket a tombbe, tokjo \o/, mar csak a kiiratast kel megoldani =)

--- fajlos.adb
+++ fajlos.adb
@@ -28,7 +28,7 @@ end record;
 --
 
 type direntrys is array (positive range <>) of direntry;
-detry: direntry;
+--detry: direntry;
 number_of_entrys:integer;
 --str:string(1..100);
 --utolso: integer;
@@ -41,7 +41,7 @@ begin
 
      declare
      
-          entrys:direntrys(1..number_of_entrys);
+          array_of_entrys:direntrys(1..number_of_entrys);
           entry_type: integer;
           file_type_of_entry: file_type;
 
@@ -49,20 +49,37 @@ begin
           Text_io.put_line("Toltsuk fel az adatokkal tombot!");
           for i in 1..number_of_entrys
           loop
-               Text_io.put("Milyen tipust szeretnek? (1-directory, 2-file): ") ;
+               Text_io.put("Milyen tipust szeretnek? (1-file, 2-directory): ") ;
                ada.integer_text_io.get(entry_type);
+               Text_io.skip_line;
                if entry_type = 1
                then
-                   file_type_of_entry := directory;
+                   file_type_of_entry := file;
 
                else
-                   file_type_of_entry := file;
+                   file_type_of_entry := directory;
 
                end if;
                declare
                     ent: direntry(file_type_of_entry);
                begin
-                   null; 
+                    Text_io.put("Adjuk meg a bejegyzes nevet: ");
+                    Text_io.get_line(ent.name,ent.name_l);
+                    Text_io.put("Adjuk meg a bejegyzes utolso modositasanak datumat: ");
+                    Text_io.get_line(ent.moddate,ent.moddate_l);
+                    Text_io.put("Adjuk meg a bejegyzes jogosultsagait: ");
+                    Text_io.get_line(ent.perm,ent.perm_l);
+                    if entry_type = 1
+                    then
+                         Text_io.put("Adjuk meg a fajl meretet: ");
+                         ada.integer_text_io.get(ent.size);
+                         Text_io.skip_line;
+                    end if;
+                    
+                    array_of_entrys(i):=ent;                   
+                    --Text_io.put_line(array_of_entrys(i).name(1..array_of_entrys(i).name_l));
+
+
                end;
                
                
@@ -79,9 +96,9 @@ begin
 
 
 
-Text_io.skip_line;
-Text_io.get_line(detry.name, detry.name_l);
-Text_io.put_line(detry.name(1..detry.name_l));
+--xt_io.skip_line;
+--Text_io.get_line(detry.name, detry.name_l);
+--Text_io.put_line(detry.name(1..detry.name_l));
 
 
 end fajlos;
