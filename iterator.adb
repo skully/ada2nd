7a1542725432ad7598e3eaf34ff08e26f65c6f0a
skully <skully@elte.hu>
2010-11-07 11:19:03 +0100

    most mar tenyleg kesz

--- iterator.adb
+++ iterator.adb
@@ -17,6 +17,17 @@ end has_next;
 procedure next (i:in out integer; array_of_entrys:in direntrys) is
 
 begin
+                    
+                    ada.Text_io.put(array_of_entrys(i).perm(1..array_of_entrys(i).perm_l) & Ada.Characters.Latin_1.HT);
+
+                    ada.Text_io.put(array_of_entrys(i).moddate(1..array_of_entrys(i).moddate_l) & Ada.Characters.Latin_1.HT & array_of_entrys(i).name(1..array_of_entrys(i).name_l) & Ada.Characters.Latin_1.HT) ;
+                    if array_of_entrys(i).ft = file
+                    then
+                         ada.Text_io.put(integer'image(array_of_entrys(i).size));
+
+                    end if;
+                    ada.Text_io.new_line;
+
 
     i:=i+1;
 
